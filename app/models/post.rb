523aa0d3880f70b7a30a6e98461e67a6ae291343
belalgamal46 <belalgamal00@gmail.com>
2023-06-08 19:05:25 +0300

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'posts_id'
   has_many :likes, foreign_key: 'posts_id'
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def five_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
