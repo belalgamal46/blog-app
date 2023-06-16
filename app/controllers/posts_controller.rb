02cc071cc091c5c9447652610a630afea6fa87d7
belalgamal46 <belalgamal00@gmail.com>
2023-06-16 22:19:07 +0300

    Update post controller to include new and create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,27 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    find_post(:id)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    if @post.save
+      flash[:notice] = 'Your post was created successfully'
+      redirect_to user_post_path(@post.author, @post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
