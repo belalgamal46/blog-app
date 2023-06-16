14a3dadd26ae740ca412ffb8ece72e9077b19b06
belalgamal46 <belalgamal00@gmail.com>
2023-06-16 22:21:33 +0300

    Create likes and comments controllers

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+    @comment.author = find_user(:user_id)
+    @comment.posts = find_post(:post_id)
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.author = find_user(:user_id)
+    @comment.posts = find_post(:post_id)
+
+    if @comment.save
+      flash[:notice] = 'Comment created successfully'
+      puts "success"
+      redirect_to user_post_path(params[:user_id], params[:post_id])
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,13 @@
+class LikesController < ApplicationController
+  def create
+    @like = Like.new
+    @like.author = find_user(:user_id)
+    @like.posts = find_post(:post_id)
+
+    if @like.save
+      redirect_to user_post_path(params[:user_id], params[:post_id])
+    else
+      render user_post_path(params[:user_id], params[:post_id]), status: :unprocessable_entity
+    end
+  end
+end
