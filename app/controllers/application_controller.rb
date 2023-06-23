d018a734814e74ae2bf6b5e3b31f5246416e83ef
belalgamal46 <belalgamal00@gmail.com>
2023-06-23 15:21:18 +0300

    Add devise to authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,24 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @user = User.first
-  end
+  before_action :authenticate_user!
+  before_action :update_allowed_parameters, if: :devise_controller?
 
-  def find_user(user)
-    @user = User.find(params[user])
+
+  def find_user
+    @user = current_user
   end
 
   def find_post(post)
     @post = Post.find(params[post])
   end
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :photo, :bio, :email, :password, :current_password)
+    end
+  end
+
+
 end
