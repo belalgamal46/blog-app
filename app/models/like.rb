cca4e3e062ee4132da41974887df18e50d3eedc9
belalgamal46 <belalgamal00@gmail.com>
2023-06-24 03:01:48 +0300

    Update likes model

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,8 +3,13 @@ class Like < ApplicationRecord
   belongs_to :posts, class_name: 'Post'
 
   after_save :update_likes_counter
+  after_destroy :update_likes_counter
 
   def update_likes_counter
-    posts.increment!(:likes_counter, 1)
+    if destroyed?
+      posts.decrement!(:likes_counter, 1)
+    else
+      posts.increment!(:likes_counter, 1)
+    end
   end
 end
